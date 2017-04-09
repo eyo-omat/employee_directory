04d38b78059a9b5afc8fb46a95797cb3d2043da5
Eyioyo <eyo.omatgmail.com>
2017-04-09 17:56:07 +0000

    fix notification typo

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -14,8 +14,8 @@ var app = {
     },
 
     showAlert: function(message, title){
-        if (navigator.notifiation){
-            navigator.notfication.alert(message, null, title, 'OK');
+        if (navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
         }
