84dee0a81b17df65268445906bd427e5c8cabc6f
Eyioyo <eyo.omatgmail.com>
2017-04-10 11:42:32 +0000

    add save new contact through contact API

--- phonegap-workshop-master/js/EmployeeView.js
+++ phonegap-workshop-master/js/EmployeeView.js
@@ -3,6 +3,7 @@ var EmployeeView = function(employee){
     this.initialize = function(){
         this.el = $('<div />');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
 
 
@@ -22,6 +23,24 @@ var EmployeeView = function(employee){
                 alert('Error getting Location');
             }
         );
+        return false;
+    };
+
+    this.addToContatcs = function(event){
+        event.preventDefault();
+        console.log('add location');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); //prefered phone number
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
         return false;
     };
 
